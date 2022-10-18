7f5482414086089fbea7b85ceebcf29f7e320891
Lydia Alem <107647071+LydiaAlem@users.noreply.github.com>
2022-10-17 19:12:38 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,12 +1,18 @@
-public class Board {
+ppublic class Board {
 
     // Instance variables
     private Piece[][] board;
+    int col;
+    int row;
+    //Piece piece;
+    private Piece piece;
 
+    
     //TODO:
     // Construct an object of type Board using given arguments.
     public Board() {
-
+        this.board = new Piece[8][8];
+        
     }
 
     // Accessor Methods
@@ -14,13 +20,15 @@ public class Board {
     //TODO:
     // Return the Piece object stored at a given row and column
     public Piece getPiece(int row, int col) {
-        return null;
+        return board[row][col];
     }
 
     //TODO:
     // Update a single cell of the board to the new piece.
     public void setPiece(int row, int col, Piece piece) {
-
+        this.row = row;
+        this.col = col;
+        this.piece = piece;
     }
 
     // Game functionality methods
@@ -29,6 +37,9 @@ public class Board {
     // Moves a Piece object from one cell in the board to another, provided that
     // this movement is legal. Returns a boolean to signify success or failure.
     public boolean movePiece(int startRow, int startCol, int endRow, int endCol) {
+        if (board[startRow][startCol].isMoveLegal(this, endRow, endCol)){
+            return true;
+        }
         return false;
     }
 
@@ -36,19 +47,33 @@ public class Board {
     // Determines whether the game has been ended, i.e., if one player's King
     // has been captured.
     public boolean isGameOver() {
-        return false;
+
+        int counter = 0;
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                if(board[i][j].getCharacter() == '\u2654' || board[i][j].getCharacter() == '\u265a')
+                    counter++;
+                }
+       if(counter != 2){
+            return true;
+
     }
+        return false;
+}
+    
 
     //TODO:
     // Construct a String that represents the Board object's 2D array. Return
     // the fully constructed String.
     public String toString() {
+
         return null;
     }
 
     //TODO:
     // Sets every cell of the array to null. For debugging and grading purposes.
     public void clear() {
+        this.board[row][col] = null;
 
     }
 
@@ -62,6 +87,14 @@ public class Board {
     // - Player's color and color of 'start' Piece match.
     // - Destination contains either no Piece or a Piece of the opposite color.
     public boolean verifySourceAndDestination(int startRow, int startCol, int endRow, int endCol, boolean isBlack) {
+        
+        
+
+
+
+
+
+
         return false;
     }
 
@@ -96,6 +129,8 @@ public class Board {
     // - All spaces directly between 'start' and 'end' are empty, i.e., null.
     public boolean verifyDiagonal(int startRow, int startCol, int endRow, int endCol) {
         return false;
+    
     }
+}
 }
 
