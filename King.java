b64d5ed6588ee86084e561264d4aa869bf0348f2
Lydia Alem <107647071+LydiaAlem@users.noreply.github.com>
2022-10-18 19:26:37 -0500

    Update King.java

--- King.java
+++ King.java
@@ -1,1 +1,23 @@
+public class King {
 
+    private int row;
+    private int col;
+    private boolean isBlack;
+
+    public King(int row, int col, boolean isBlack){
+        this.row = row;
+        this.col = col;
+        this.isBlack = isBlack;
+    }
+
+    public boolean isMoveLegal(Board board, int endRow, int endCol){
+        if (board.verifyDiagonal(this.row, this.col, endRow, endCol) && board.getPiece(endRow, endCol) == null) {
+            // Case 1: Forward movement to empty square.
+            if (this.isBlack) {
+                return (endRow == this.row + 1) || ((endRow == this.row + 2) && (this.row == 1));
+            } else {
+                return (endRow == this.row - 1) || ((endRow == this.row - 2) && (this.row == 6));
+            }
+}
+//Not completed YET
+      
