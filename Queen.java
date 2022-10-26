e4a6efb9f0a374b661340f10230eea8edbb9f10b
Lydia Alem <107647071+LydiaAlem@users.noreply.github.com>
2022-10-26 09:32:07 -0500

    Update Queen.java

--- Queen.java
+++ Queen.java
@@ -1,1 +1,21 @@
+public class Queen {
+    //Instance variables
+    private int row;
+    private int col;
+    private boolean isBlack;
+
+    //Constructer method
+    public Queen(int row, int col, boolean isBlack){
+        this.row = row;
+        this.col = col;
+        this.isBlack = isBlack;
+    }
+
+    public boolean isMoveLegal(Board board, int endRow, int endCol){
+        if(board.verifySourceAndDestination(this.row, this.col, endRow, endCol, isBlack)){
+            return(board.verifyDiagonal(this.row, this.col, endRow, endCol));
+        }
+        return false;
+    }
+}
 
