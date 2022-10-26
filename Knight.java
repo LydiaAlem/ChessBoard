42916e05d3095c29f0ddd6d1e2ed6c59ce5e9412
Lydia Alem <107647071+LydiaAlem@users.noreply.github.com>
2022-10-26 09:29:17 -0500

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -1,1 +1,41 @@
+public class Knight {
+    //Instance
+    private int row;
+    private int col;
+    private boolean isBlack;
 
+    //Constructer:
+    public Knight(int row, int col, boolean isBlack){
+        this.row= row;
+        this.col = col;
+        this.isBlack = isBlack;
+    }
+
+    public boolean isMoveLegal(Board board, int endRow, int endCol){
+        if(board.verifySourceAndDestination(this.row, this.col, endRow, endCol, this.isBlack)){
+            if(((this.row + 2) == endRow ) && (this.col -1 == endCol)){
+                return true; //Checks bottom left
+            }
+            if(((this.row + 1) == endRow && this.col -2 == endCol)){
+                return true; //Checks lower left
+            }
+            if(((this.row - 1) == endRow && this.col - 2 == endCol)){
+                return true; //Checks upper left
+            }
+            if(((this.row - 2) == endRow && this.col - 1 == endCol)){
+                return true; //Checks top left
+            }
+            if(((this.row - 1) == endRow && this.col + 2 == endCol)){
+                return true; //Checks top right
+            }
+            if(((this.row + 2) == endRow && this.col + 1 == endCol)){
+                return true; //Checks mid right
+            }
+            if(((this.row + 1) == endRow && this.col + 2 == endCol)){
+                return true; //Checks bottom right
+            }
+        }
+        return false;
+    
+    }
+}
