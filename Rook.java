96cf30bc6ff810afc877fb66d4606e9bba9972b2
Lydia Alem <107647071+LydiaAlem@users.noreply.github.com>
2022-10-26 09:14:50 -0500

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -1,1 +1,22 @@
+public class Rook {
+    //Instance variables
+    private int row;
+    private int col;
+    private boolean isBlack;
 
+    //Constructer
+    public Rook(int row, int col, boolean isBlack){
+        this.row = row;
+        this.col = col;
+        this.isBlack = isBlack;
+    }
+
+    public boolean isMoveLegal(Board board, int endRow, int endCol){
+        if(board.verifySourceAndDestination(this.row, this.col, endRow, endCol, this.isBlack)){
+            return(board.verifyHorizontal(this.row, this.col, endRow, endCol)|| board.verifyVertical(this.row, this.col, endRow, endCol));
+        }
+        else{
+            return false;
+        }
+    }
+}
