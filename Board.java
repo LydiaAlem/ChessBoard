9260d50beedbc9b03f96f1f78e8a9b7037f9268b
Lydia Alem <107647071+LydiaAlem@users.noreply.github.com>
2022-10-28 10:35:15 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,31 +1,18 @@
 public class Board {
-    // Instance variables
     private Piece[][] board;
 
-    //TODO:
-    // Construct an object of type Board using given arguments.
     public Board() {
         this.board = new Piece[8][8];
     }
 
-    // Accessor Methods
-    //TODO:
-    // Return the Piece object stored at a given row and column
     public Piece getPiece(int row, int col) {
         return this.board[row][col];
     }
 
-    //TODO:
-    // Update a single cell of the board to the new piece.
     public void setPiece(int row, int col, Piece piece) {
         this.board[row][col] = piece;
     }
 
-    // Game functionality methods
-
-    //TODO:
-    // Moves a Piece object from one cell in the board to another, provided that
-    // this movement is legal. Returns a boolean to signify success or failure.
     public boolean movePiece(int startRow, int startCol, int endRow, int endCol) {
         if (this.board[startRow][startCol] != null && this.board[startRow][startCol].isMoveLegal(this, endRow, endCol)) {
             board[endRow][endCol] = board[startRow][startCol];
@@ -36,9 +23,6 @@ public class Board {
         return false;
     }
 
-    //TODO:
-    // Determines whether the game has been ended, i.e., if one player's King
-    // has been captured.
     public boolean isGameOver() {
 
         int counter = 0;
@@ -55,10 +39,6 @@ public class Board {
         return false;
     }
 
-    //TODO:
-    // Construct a String that represents the Board object's 2D array. Return
-    // the fully constructed String.
-    //How do i do this???
     public String toString() {
         String map = "Board: " + "\n" + "   0 1 2 3 4 5 6 7 " + "\n";
         for(int i=0; i<= 7; i++){
@@ -86,9 +66,6 @@ public class Board {
         }
     }
 
-    // Movement helper functions
-    
-//Finished
     public boolean verifySourceAndDestination(int startRow, int startCol, int endRow, int endCol, boolean isBlack) {
         if(startRow <= 7 && startCol <= 7 && endRow <= 7 & endCol <= 7){ //Checks bounds check if greater than or equal to 0
             if(board[startRow][startCol].equals(null)){  //Checks empty piece
@@ -101,11 +78,8 @@ public class Board {
             }
         return false;
     }
-    
-    //TODO:
-    // Check whether the 'start' position and 'end' position are adjacent to each other
     public boolean verifyAdjacent(int startRow, int startCol, int endRow, int endCol) {
-        //to help others use switch statement
+
         
         if(board[startRow][startCol + 1] == (board[endRow][endCol])){ //Check right
             return true;
@@ -144,7 +118,6 @@ public class Board {
         }
     }
 
-    // - All spaces directly between 'start' and 'end' are empty, i.e., null.
     public boolean verifyHorizontal(int startRow, int startCol, int endRow, int endCol) {
         for(int i = 0; i < board.length; i++){
                 if((board[endRow][endCol + i]) != null || (board[endRow][endCol - i] != null)){
@@ -155,7 +128,6 @@ public class Board {
     return false; 
     }
 
-    // - All spaces directly between 'start' and 'end' are empty, i.e., null.
     public boolean verifyVertical(int startRow, int startCol, int endRow, int endCol) {
         for(int i = 0; i < board.length; i++){
             if((board[endRow + i][endCol]) != null|| (board[endRow - i][endCol] != null)){
@@ -164,8 +136,7 @@ public class Board {
         }
         return false;
     }
-    
-    // - All spaces directly between 'start' and 'end' are empty, i.e., null.
+
     public boolean verifyDiagonal(int startRow, int startCol, int endRow, int endCol) {
         for(int i = 0; i < board.length;i++){
             for(int j = 0; j< board.length; j++){
