d7de95d2edf952fc3cd6a7efd303389d44b28c22
Lydia Alem <107647071+LydiaAlem@users.noreply.github.com>
2022-10-28 03:41:26 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,47 +1,47 @@
-public static void main(String[] args) {
+import java.util.Scanner;
+public class Game {
+    public static void main(String[] args) {
+    
+        //Testing board:
         Board gameBoard = new Board();
         boolean isBlack = false;
         Fen.load("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", gameBoard);
-        System.out.print(gameBoard.toString());
+        System.out.println(gameBoard.toString());
+        Scanner input = new Scanner(System.in);
+
         while(gameBoard.isGameOver() == false){
             //implement all methods for game to work
-            Scanner input = new Scanner(System.in);
-            System.out.print("What is your move? (format: [startRow][startCol][endRow][endCol]: ");
+
+            //CHECKS WHO'S TURN IT IS
+            isBlack = !isBlack;
+            if(isBlack == false){
+                System.out.println("\nIt's now whites turn.");
+            }else{
+                System.out.println("\nIt's now black's turn.");
+            }
+
+            System.out.print("What is your move? (format: [startRow][startCol][endRow][endCol]): ");
             int startRow = input.nextInt();
             int startCol = input.nextInt();
             int endRow = input.nextInt();
             int endCol = input.nextInt();
-            while(gameBoard.movePiece(startRow, startCol, endRow, endCol) == false){
-                Scanner input1 = new Scanner(System.in);
-                System.out.print("Invalid input! You can not move here. Please enter a new move: ");
-                startRow = input.nextInt();
-                startCol = input.nextInt();
-                endRow = input.nextInt();
-                endCol = input.nextInt();
-                input1.close();
+            //gameBoard.movePiece(startRow, startCol, endRow, endCol);
+
+            if(gameBoard.movePiece(startRow, startCol, endRow, endCol) == true){
+                //Scanner input1 = new Scanner(System.in);
+                // startRow = input.nextInt();
+                // startCol = input.nextInt();
+                // endRow = input.nextInt();
+                // endCol = input.nextInt();
+                //input1.close();
+                System.out.println("Great move!");
+                System.out.println(gameBoard.toString());
                 gameBoard.movePiece(startRow, startCol, endRow, endCol);
-            }
-            isBlack = !isBlack;
-            if(isBlack == false){
-                System.out.println("It's now whites turn.");
-                // Scanner input2 = new Scanner(System.in);
-                // System.out.print("What is your move? (format: [startRow][startCol][endRow][endCol]: ");
-                // startRow = input.nextInt();
-                // startCol = input.nextInt();
-                // endRow = input.nextInt();
-                // endCol = input.nextInt();
-                // input2.close();
-                // gameBoard.movePiece(startRow, startCol, endRow, endCol);
             }else{
-                System.out.println("It's not black's turn.");
-                // Scanner input3 = new Scanner(System.in);
-                // System.out.print("What is your move? (format: [startRow][startCol][endRow][endCol]: ");
-                // startRow = input.nextInt();
-                // startCol = input.nextInt();
-                // endRow = input.nextInt();
-                // endCol = input.nextInt();
-                // input3.close();
-                // gameBoard.movePiece(startRow, startCol, endRow, endCol);
+                System.out.println("Invalid input! You can not move here. Please enter a new move: ");
+                System.out.println(gameBoard.toString());
             }
-            input.close();
-        } 
+        }
+    input.close();
+}
+}
