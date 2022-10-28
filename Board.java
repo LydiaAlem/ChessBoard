4367437acd0d422216592c2a4a7761da494ca539
bilesed <113912563+bilesed@users.noreply.github.com>
2022-10-27 20:13:47 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -2,17 +2,13 @@ public class Board {
 
     // Instance variables
     private Piece[][] board;
-    int col;
     int row;
-    //Piece piece;
-    private Piece piece;
+    int col;
 
-    
     //TODO:
     // Construct an object of type Board using given arguments.
     public Board() {
-        this.board = new Piece[7][7];
-        
+        this.board = new Piece[8][8];
     }
 
     // Accessor Methods
@@ -28,7 +24,6 @@ public class Board {
     public void setPiece(int row, int col, Piece piece) {
         this.row = row;
         this.col = col;
-        this.piece = piece;
     }
 
     // Game functionality methods
@@ -37,7 +32,8 @@ public class Board {
     // Moves a Piece object from one cell in the board to another, provided that
     // this movement is legal. Returns a boolean to signify success or failure.
     public boolean movePiece(int startRow, int startCol, int endRow, int endCol) {
-        if (board[startRow][startCol].isMoveLegal(this, endRow, endCol)){
+        if(board[startRow][startCol].isMoveLegal(this,endRow,endCol)) {
+            board[startRow][startCol ]= board[endRow][endCol];
             return true;
         }
         return false;
@@ -47,33 +43,29 @@ public class Board {
     // Determines whether the game has been ended, i.e., if one player's King
     // has been captured.
     public boolean isGameOver() {
-
-        int counter = 0;
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){
-                if(board[i][j].getCharacter() == '\u2654' || board[i][j].getCharacter() == '\u265a')
-                    counter++;
+        int count = 0;
+        for(int i = 0; i< board.length; i++){
+            for(int j = 0; j<board[i].length;i++){
+                if(board[i][j].getCharacter() == '\u265a' || board[i][j].getCharacter() == '\u2654')
+                    count++;
                 }
-       if(counter != 2){
-            return true;
-
-    }
-        else {return false;
+            if(count != 2){
+                return true;
+            }
         }
+        return false;
     }
-    
+        
 
     //TODO:
     // Construct a String that represents the Board object's 2D array. Return
     // the fully constructed String.
-    //How do i do this???
     public String toString() {
         String map = "Board: " + "\n" + " 0 1 2 3 4 5 6 7" + "\n";
         for(int i=0; i<= 7; i++){
             map += Integer.toString(i) + "|";
             for(int j=0; j<=7; j++){
                 if(board[i][j] != (null)){ // ASK OH
-                    System.out.print('k');
                     map += board[i][j].getCharacter() + "|"; 
                 }
                 else{
@@ -82,120 +74,133 @@ public class Board {
             }
             map += "\n";
         }
-            return map;
+        return map;
     }
 
-
-    public void clear() {
-        for(int i = 0; i<= board[row].length; i++){
-            for(int j = 0; j<= board[col].length; j++){
-                board[row][col]= null;
+    //TODO:
+    // Sets every cell of the array to null. For debugging and grading purposes.
+    public void clear() {  
+        for(int i = 0; i<this.board[row].length; i++){
+            for(int j = 0; j<this.board[col].length; j++){
+               this.board[row][col] = null;
             }
         }
-    }}
+    }
 
     // Movement helper functions
-    
+
     //TODO:
     // Ensure that the player's chosen move is even remotely legal.
     // Returns a boolean to signify whether:
     // - 'start' and 'end' fall within the array's bounds.
-    // - Both contain a Piece object, i.e., not null.
+    // - 'start' contains a Piece object, i.e., not null.
     // - Player's color and color of 'start' Piece match.
-    // - Destination contains either no Piece or a Piece of the opposite color.
+    // - 'end' contains either no Piece or a Piece of the opposite color.
     public boolean verifySourceAndDestination(int startRow, int startCol, int endRow, int endCol, boolean isBlack) {
-        
-        if(board[startRow][startCol].isMoveLegal(this, endRow, endCol)){
-            //how do i place 'start' and 'end' fall within the array's bounds
+        if(startRow<= 7 && startCol<=7 && endRow<=7&& startRow<= 7){
+            if(!board[startRow][startCol].equals(null)){
+                if(board[startRow][startCol].getIsBlack() == isBlack){
+                    if(board[endRow][endCol].equals(null)|| board[endRow][endCol].getIsBlack()!= isBlack){
+                        return true;
+                    }
+                }
+            }
         }
-        else{
-            return false;
-        }
+        return false;
     }
 
     //TODO:
     // Check whether the 'start' position and 'end' position are adjacent to each other
     public boolean verifyAdjacent(int startRow, int startCol, int endRow, int endCol) {
-        //to help others use switch statement
-        
-        if(board[startRow][startCol + 1].equals(board[endRow][endCol])){ //Check right
-            return true;
-        } 
-
-        else if(board[startRow + 1][startCol + 1].equals(board[endRow][endCol])){ //Checks bottom right corner
+        if(board[startRow+1][startCol] == board[endRow][endCol]){//check below
             return true;
         }
-
-        else if(board[startRow - 1][startCol].equals(board[endRow][endCol])) {//checks right
-            return true; 
-        }
-
-        else if(board[startRow -1][startCol -1].equals(board[endRow][endCol])){ //checks upper left corner
+        if(board[startRow-1][startCol] == (board[endRow][endCol])){//check above
             return true;
         }
-
-        else if(board[startRow][startCol - 1].equals(board[endRow][endCol])){ //checks ABOVE pieve
+        if(board[startRow][startCol+1] == (board[endRow][endCol])){//check right
             return true;
         }
-
-        else if(board[startRow-1][startCol+1].equals(board[endRow][endCol])){ //checks bottem left corner
+        if(board[startRow][startCol-1] == (board[endRow][endCol])){//check left
             return true;
         }
-
-        else if(board[startRow+1][startCol - 1].equals(board[endRow][endCol])){ //checks upper right corner
+        if(board[startRow-1][startCol-1] == (board[endRow][endCol])){//up one, left one
             return true;
         }
-
-        else if(board[startRow +1][startCol].equals(board[endRow][endCol])){ //checks left
+        if(board[startRow+1][startCol+1] == (board[endRow][endCol])){//down one, right one
             return true;
         }
-
-        else {
-            return false;
+        if(board[startRow-1][startCol+1] == (board[endRow][endCol])){//up one, right one
+            return true;
         }
-
-
+        if(board[startRow+1][startCol-1] == (board[endRow][endCol])){//down one, left one
+            return true;
+        }
+        return false;
+        //within one box of each other horizontal or vertical or diagonal
     }
 
+    //TODO:
+    // Checks whether a given 'start' and 'end' position are a valid horizontal move.
+    // Returns a boolean to signify whether:
+    // - The entire move takes place on one row.
     // - All spaces directly between 'start' and 'end' are empty, i.e., null.
     public boolean verifyHorizontal(int startRow, int startCol, int endRow, int endCol) {
-        for(int i = 0; i<= startRow.length; i++){
-            for(int j= 0; j <= startCol.length; j++){
-                if((board[startRow][startCol + 1]).equals(board[endRow][endCol])|| (board[startRow][startCol -1].equals(board[endRow][endCol]))){
+        for(int i = 0; i<board.length; i++){
+            for(int j = 0; j<board[i].length; j++){
+                if(board[startRow+i][startCol] == (board[endRow][endCol])){
                     return true;
                 }
-                else{ return false; }
             }
         }
+        return false;
     }
 
-
+    //TODO:
+    // Checks whether a given 'start' and 'end' position are a valid vertical move.
+    // Returns a boolean to signify whether:
+    // - The entire move takes place on one column.
     // - All spaces directly between 'start' and 'end' are empty, i.e., null.
     public boolean verifyVertical(int startRow, int startCol, int endRow, int endCol) {
-        for(int i = 0; i<= startRow.length; i++){
-            for(int j= 0; j <= startCol.length; j++){
-                if((board[startRow + 1][startCol]).equals(board[endRow][endCol]) || (board[startRow - 1][startCol].equals(board[endRow][endCol]))){
+        for(int i = 0; i<board.length; i++){
+            for(int j = 0; j<board[i].length; j++){
+                if(board[startRow][startCol+i] == (board[endRow][endCol])){
                     return true;
                 }
-                else{ return false; }
             }
         }
+        return false;
     }
 
+    //TODO:
+    // Checks whether a given 'start' and 'end' position are a valid diagonal move.
+    // Returns a boolean to signify whether:
+    // - The path from 'start' to 'end' is diagonal... change in row and col.
     // - All spaces directly between 'start' and 'end' are empty, i.e., null.
     public boolean verifyDiagonal(int startRow, int startCol, int endRow, int endCol) {
-        for(int i = 0; i<= startRow.length;i++){
-            for(int j= 0; j <= startCol.length; j++){
-                if((board[startRow + 1][startCol + 1]).equals(board[endRow][endCol])|| (board[startRow + 1][startCol + 1].equals(board[endRow][endCol]))){
+        for(int i = 0; i<board.length; i++){
+            for(int j = 0; j<board[i].length; j++){
+                if(board[startRow+i][startCol+i] == (board[endRow][endCol])){
                     return true;
-                }
-                else {
-                    return false;
-                }
                 }
             }
         }
-    }     
+        return false;
+    }
+    // public booelan check_piece(){
+    //     Board gameBoard = new Board();
+    //     if(gameBoard.getPiece(row, col).equals('\u2659')){
+    //         gameBoard.getPiece(row, col).promotion();
+    //         return true;
+    //     }
+    //     if(gameBoard.getPiece(row, col).equals( '\u265f')){
+    //         gameBoard.getPiece(row, col).promotion();
+    //         return true;
+    //     }
+    
+    //         // gameBoard.getPiece() == '\u265f'
+    //     return false;
+    // }
+     
 }
-
 
