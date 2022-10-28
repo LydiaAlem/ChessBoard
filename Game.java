735d557914499806fade098da88032b611e349d3
Lydia Alem <107647071+LydiaAlem@users.noreply.github.com>
2022-10-28 10:36:37 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 public class Game {
     public static void main(String[] args) {
-    
-        //Testing board:
+   
         Board gameBoard = new Board();
         boolean isBlack = false;
         Fen.load("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", gameBoard);
@@ -25,15 +24,8 @@ public class Game {
             int startCol = input.nextInt();
             int endRow = input.nextInt();
             int endCol = input.nextInt();
-            //gameBoard.movePiece(startRow, startCol, endRow, endCol);
-
+            
             if(gameBoard.movePiece(startRow, startCol, endRow, endCol) == true){
-                //Scanner input1 = new Scanner(System.in);
-                // startRow = input.nextInt();
-                // startCol = input.nextInt();
-                // endRow = input.nextInt();
-                // endCol = input.nextInt();
-                //input1.close();
                 System.out.println("Great move!");
                 System.out.println(gameBoard.toString());
                 gameBoard.movePiece(startRow, startCol, endRow, endCol);
